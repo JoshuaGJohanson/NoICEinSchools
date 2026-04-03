deedce45776e8792edef09d079fbee4185126e37
Joshua Johanson <joshua.johanson@gmail.com>
2026-04-03 15:30:41 -0600

    states and utah

--- assets/js/subscription-form.js
+++ assets/js/subscription-form.js
@@ -4,6 +4,26 @@
 // Usage:
 //   initSubscriptionForm({ stateSelect, districtInput, datalist, activityNote });
 
+var statePages = {
+  'Alabama': '/states/al.html', 'Alaska': '/states/ak.html', 'Arizona': '/states/az.html',
+  'Arkansas': '/states/ar.html', 'California': '/states/ca.html', 'Colorado': '/states/co.html',
+  'Connecticut': '/states/ct.html', 'Delaware': '/states/de.html', 'Florida': '/states/fl.html',
+  'Georgia': '/states/ga.html', 'Hawaii': '/states/hi.html', 'Idaho': '/states/id.html',
+  'Illinois': '/states/il.html', 'Indiana': '/states/in.html', 'Iowa': '/states/ia.html',
+  'Kansas': '/states/ks.html', 'Kentucky': '/states/ky.html', 'Louisiana': '/states/la.html',
+  'Maine': '/states/me.html', 'Maryland': '/states/md.html', 'Massachusetts': '/states/ma.html',
+  'Michigan': '/states/mi.html', 'Minnesota': '/states/mn.html', 'Mississippi': '/states/ms.html',
+  'Missouri': '/states/mo.html', 'Montana': '/states/mt.html', 'Nebraska': '/states/ne.html',
+  'Nevada': '/states/nv.html', 'New Hampshire': '/states/nh.html', 'New Jersey': '/states/nj.html',
+  'New Mexico': '/states/nm.html', 'New York': '/states/ny.html', 'North Carolina': '/states/nc.html',
+  'North Dakota': '/states/nd.html', 'Ohio': '/states/oh.html', 'Oklahoma': '/states/ok.html',
+  'Oregon': '/states/or.html', 'Pennsylvania': '/states/pa.html', 'Rhode Island': '/states/ri.html',
+  'South Carolina': '/states/sc.html', 'South Dakota': '/states/sd.html', 'Tennessee': '/states/tn.html',
+  'Texas': '/states/tx.html', 'Utah': '/states/ut/', 'Vermont': '/states/vt.html',
+  'Virginia': '/states/va.html', 'Washington': '/states/wa.html', 'West Virginia': '/states/wv.html',
+  'Wisconsin': '/states/wi.html', 'Wyoming': '/states/wy.html'
+};
+
 var highActivity = [
   'Arizona','California','Colorado','Florida','Georgia','Illinois','Maryland',
   'Massachusetts','Mississippi','Nevada','New Jersey','New York','North Carolina',
@@ -25,7 +45,7 @@ function initSubscriptionForm(els) {
     var s = this.value;
 
     if (!s) {
-      activityNote.textContent = '';
+      activityNote.innerHTML = '';
       activityNote.classList.remove('visible');
       datalist.innerHTML = '';
       districtInput.placeholder = 'e.g. Los Angeles Unified School District';
@@ -40,7 +60,11 @@ function initSubscriptionForm(els) {
     } else {
       msg = 'Campaign activity in ' + s + ' varies by district. State and local updates will help you track what is happening near you.';
     }
-    activityNote.textContent = msg;
+    var page = statePages[s];
+    if (page) {
+      msg += ' <a href="' + page + '">View the ' + s + ' page →</a>';
+    }
+    activityNote.innerHTML = msg;
     activityNote.classList.add('visible');
 
     datalist.innerHTML = '';
